00181639748b16ff9f59305c1dc906bdce617128
isangbin <isangbin96@gmail.com>
2022-11-17 17:37:45 +0900

    recommend_backend_isangbin

--- movies/views.py
+++ movies/views.py
@@ -3,20 +3,44 @@ from .models import Movie
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
+import operator
 # import requests
 # import json
 
 # Create your views here.
 def index(request):
-    movies = Movie.objects.all()
-    # User = get_user_model()
-    # pickedmovies = User.objects.get(pk=user_pk)
-    pickedmovies = request.user.pk
-    context = {
-        'movies': movies,
-        'pickedmovies': pickedmovies,
-    }
-    return render(request, 'movies/index.html', context)
+    if request.user.is_authenticated:
+        movies = Movie.objects.all()
+        User = get_user_model()
+        user = User.objects.get(pk=request.user.pk)
+        pickedmovies = user.like_movies.all()
+
+        genre_ids = {28: 0, 12: 0, 16: 0, 35: 0, 80: 0, 99: 0, 18: 0, 10751: 0, 14: 0, 36: 0, 27: 0, 10402: 0, 9648: 0, 10749: 0, 878: 0, 10770: 0, 53: 0, 10752: 0, 37: 0}
+        for pickedmovie in pickedmovies:
+            for genre in pickedmovie.genres.all():
+                genre_ids[genre.id] += 1
+        
+        max_genre = max(genre_ids.items(), key=operator.itemgetter(1))[0]
+
+        # recommends = Movie.objects.filter(genres__contains=max_genre)
+        recommends = []
+
+        for movie in movies:
+            for genre in movie.genres.all():
+                if max_genre == genre.id:
+                    recommends.append(movie)
+                    break
+        
+
+        context = {
+            'movies': movies,
+            'pickedmovies': pickedmovies,
+            'genre_ids': genre_ids,
+            'max_genre': max_genre,
+            'recommends': recommends,
+        }
+        return render(request, 'movies/index.html', context)
+    return redirect('accounts:login')
 
 
 def detail(request, movie_pk):
